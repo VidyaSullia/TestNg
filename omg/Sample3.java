39c6d7b0a57ffec4ba331f93e3896f3e237ebce2
vidya123 <vidya@gmail.com>
2020-04-02 17:19:02 +0530

    check

--- omg/Sample3.java
+++ omg/Sample3.java
@@ -2,12 +2,18 @@ package omg;
 
 import org.testng.annotations.Test;
 
-public class Sample3{
+public class Sample3 {
 	@Test
 	public void aTest() {
 		System.out.println("A");
 		System.out.println();
 
 	}
-}
 
+	@Test
+	public void sTest() {
+		System.out.println("S");
+		System.out.println();
+
+	}
+}
